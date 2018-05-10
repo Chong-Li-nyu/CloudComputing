18bee35f8903ab94c32a68fe1ba54407115d2482
Chong-Li-nyu <cl4056@nyu.edu>
2018-05-10 17:17:05 -0400

    add multiple select UI:

---
+++ wish-list/src/components/CheckItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+export default class CheckItem extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      checked: false,
+    };
+    this.handleClick = this.handleClick.bind(this);    
+  }
+
+  validate(chosenIds){
+    if(this.state.checked){
+      chosenIds.push(chosenIds);
+    }
+  }
+
+  handleClick (e){
+    this.setState({
+      checked: !this.state.checked
+    });
+  }
+
+  render (){
+    // let text = this.state.checked ? <strike>{this.props.message}</strike> : this.props.message;
+    let text = this.props.info;
+    let item = (
+      <label><input type="checkbox" onClick={this.handleClick} />&nbsp;{text}</label>
+    );
+    return (
+        <div className="checkItemBox">
+          {item}
+        </div>
+    );
+  }
+}
---
+++ wish-list/src/components/CheckList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Jumbotron, Button, Grid, Row, Col} from 'react-bootstrap';
+import CheckItem from './CheckItem.jsx';
+
+class CheckList extends React.Component{
+  constructor(props){
+    super(props);
+    this.submitInput = this.submitInput.bind(this);
+    this.chosenIds = [];
+  }
+
+  submitInput(event){
+    this.childrenClone.forEach((el) => {
+      var r = el.ref; //use the ref to access the child's methods
+      this.refs[r].validate(this.chosenIds);
+    });
+    console.log(this.chosenIds);
+    this.chosenIds = [];
+    event.preventDefault();
+  }
+
+  render() {
+    this.childrenClone = React.Children.map(this.props.children,
+     (child) => React.cloneElement(child, {
+       ref: Math.random().toString(36).slice(-5) //add a random string as a ref
+     })
+    );
+    return (
+    <Jumbotron>
+      <Grid>
+      <form onSubmit={this.submitInput}>
+       {this.childrenClone}
+          <div className="buttonBox">
+          <Button type="submit" bsStyle='success'>Submit</Button>
+          </div>
+      </form>
+      </Grid>
+    </Jumbotron>
+    );
+  }
+}
+
+export default CheckList;
